ca183c9ccc8fbdff6c3b17e2c9710411a8d54ef4
Unknown <andreymendozaf@gmail.com>
2017-11-22 10:57:33 -0600

    Buscar directorio

--- Back-End/WebService/src/java/Controller/ArchivoController.java
+++ Back-End/WebService/src/java/Controller/ArchivoController.java
@@ -1,10 +1,12 @@
 
 package Controller;
 
+import Model.Carpeta;
 import Model.MensajeModel;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 
 @Path("Archivo")
@@ -14,7 +16,7 @@ public class ArchivoController {
     
     @Path("crear_carpeta")
     @GET
-    public String crear_carpeta(
+    public Response crear_carpeta(
             @QueryParam("usuario") String usuario,
             @QueryParam("ruta") String ruta,
             @QueryParam("nombre") String nombre)
@@ -25,21 +27,21 @@ public class ArchivoController {
             {
                 mensaje.setMensaje("OK");
                 mensaje.addObjeto(Herramientas.buscar_directorio(usuario, ruta));
-                return Herramientas.crear_json(mensaje);
             }
             else
                 mensaje.setMensaje("No fue posible agregar la carpeta.");
         } catch (Exception ex) {
             mensaje.setMensaje("ERROR");
-        }        
-        return Herramientas.crear_json(mensaje);
+        }
+        String output = Herramientas.crear_json(mensaje);
+        return Response.ok(output).header("Access-Control-Allow-Origin", "*").build();
     }
   
 // -----------------------------------------------------------------------------
 
     @Path("crear_archivo")
     @GET
-    public String agregar_archivo(
+    public Response agregar_archivo(
             @QueryParam("usuario") String usuario,
             @QueryParam("ruta") String ruta,
             @QueryParam("nombre") String nombre,
@@ -52,15 +54,40 @@ public class ArchivoController {
             {
                 mensaje.setMensaje("OK");
                 mensaje.addObjeto(Herramientas.buscar_directorio(usuario, ruta));
-                return Herramientas.crear_json(mensaje);
             }
             else
                 mensaje.setMensaje("No fue posible agregar el archivo.");
         } catch (Exception ex) {
             mensaje.setMensaje("ERROR");
         }        
-        return Herramientas.crear_json(mensaje);
+        String output = Herramientas.crear_json(mensaje);
+        return Response.ok(output).header("Access-Control-Allow-Origin", "*").build();
     }
    
+// -----------------------------------------------------------------------------
+
+    @Path("buscar_directorio")
+    @GET
+    public Response buscar_directorio(
+            @QueryParam("usuario") String usuario,
+            @QueryParam("ruta") String ruta)
+    {
+        MensajeModel mensaje = new MensajeModel();
+        try {
+            Carpeta directorio = Herramientas.buscar_directorio(usuario, ruta);
+            if (directorio != null)
+            {
+                mensaje.setMensaje("OK");
+                mensaje.addObjeto(directorio);
+            }
+            else
+                mensaje.setMensaje("Directorio no encontrado.");
+        } catch (Exception ex) {
+            mensaje.setMensaje("ERROR");
+        }        
+        String output = Herramientas.crear_json(mensaje);
+        return Response.ok(output).header("Access-Control-Allow-Origin", "*").build();
+    }
+    
 // -----------------------------------------------------------------------------
 }
